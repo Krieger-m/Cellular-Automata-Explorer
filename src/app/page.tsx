62ef09c329131c41b8bc7abea363188e4984ef5e
Mat <matze.krieger@googlemail.com>
2026-04-04 12:43:19 +0200

    edited app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,38 @@
-import Image from "next/image";
 import styles from "./styles/page.module.css";
+import Link from "next/link";
+import { Rules } from "@/lib/automata-rules";
+import { Spacing } from "@/_components/Spacing";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      <h2>
+        A curated collection of cellular automata
+        <br />
+        with unique behaviors and visual patterns.
+      </h2>
+      <Spacing height={2} />
+
+      <p>
+        Cellular automata are simple rule‑based systems that create surprisingly
+        complex patterns.
+        <br />
+        Below you’ll find a growing library of automata, each with its own
+        personality, dynamics, and visual style.
+        <br />
+        <br />
+        Click any rule to explore how it evolves.
+      </p>
+      <Spacing height={4} />
+
+      <div style={{display: 'flex', flexDirection: 'column', rowGap: "1em", textAlign: 'center' }}>
+      {Object.entries(Rules).map(([key, rule]) => (
+          <Link key={key} href={`/${key}`} >
+            {rule.name}
+          </Link>
+      ))}
+      </div>
+      <Spacing height={2} />
     </div>
   );
 }
