64a6814d9e20ed351e30ea46d8094abea3d8fbc6
Mat <matze.krieger@googlemail.com>
2026-04-04 12:41:06 +0200

    added header

---
+++ src/_components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <>
+      <nav style={{ border: "1px solid grey", padding: 20 }}>
+        <h1>Cellular Automata Explorer</h1>
+      </nav>
+    </>
+  );
+}
