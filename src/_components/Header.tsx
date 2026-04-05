9ab9f3ac095170c9ac1661eb65f9c55d580bb92b
Mat <matze.krieger@googlemail.com>
2026-04-05 11:28:13 +0200

    edited header styles and added icon to header

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -1,9 +1,21 @@
-import styles from './styles/components.module.css'
+import Image from "next/image";
+import styles from "./styles/components.module.css";
+import icon from "../app/icon.png";
+import Link from "next/link";
 
 export function Header() {
   return (
     <>
-      <nav className={styles.header} >
+      <nav className={styles.header}>
+        <Link href="/">
+          <Image
+            src={icon.src}
+            alt="icon"
+            width="50"
+            height="50"
+            style={{ borderRadius: 25 }}
+          />
+        </Link>{" "}
         <h1>Cellular Automata Explorer</h1>
       </nav>
     </>
